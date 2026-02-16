be47a3034f149fc0b2a9a64f35855a5490303f6e
alihassan0563 <alihassan940210@gmail.com>
2026-02-16 23:55:44 +0500

    FIx email smtp conection

--- backend/server.js
+++ backend/server.js
@@ -55,10 +55,15 @@ app.use(express.static('.'));
 
 // Configure Nodemailer
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // You can change this to other services (outlook, yahoo, etc.)
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
