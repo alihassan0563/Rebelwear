ee8efd158063b5ef407c38d991f0cf9f5b5aee07
alihassan0563 <alihassan940210@gmail.com>
2026-02-22 23:23:46 +0500

    changes

--- backend/server.js
+++ backend/server.js
@@ -69,7 +69,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static("."));
 app.set("trust proxy", 1);
 
-// Configure Nodemailer
+// Configure Nodemail
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
